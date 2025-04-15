638aad7197abdb95682448027ce1023c9444451e
Salman-Sourov <salman.sourov40@gmail.com>
2025-04-15 22:33:10 +0600

    API Testing CRUD

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -9,42 +11,86 @@ List<Category> categories = new List<Category>();
 // Define routes
 app.MapGet("/", () => "API Working");
 
-app.MapGet("/api/categories", () => Results.Ok(categories));
-
-app.MapPost("/api/categories", () =>
+app.MapGet("/api/categories", ([FromQuery] string searchValue = "") =>
 {
-     var newCategory = new Category
+     // Console.WriteLine($"{searchValue}");
+     if (!string.IsNullOrEmpty(searchValue))
      {
-          CategoryId = Guid.Parse("d2719f0e-95d7-4f52-a4cc-292b34ef8b3a"),
-          Name = "Book",
-          Description = "Lorem ipsum",
-          CreatedAt = DateTime.UtcNow,
-     };
-     categories.Add(newCategory);
-     return Results.Created($"/api/categories/{newCategory.CategoryId}", newCategory);
+          var searchedCategories = categories.Where(c => !string.IsNullOrEmpty(c.Name) && c.Name.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+          return Results.Ok(searchedCategories);
+     }
+     return Results.Ok(categories);
 });
 
-app.MapDelete("/api/categories", () =>
+app.MapPost("/api/categories", (Category categoryData) =>
 {
-     var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("d2719f0e-95d7-4f52-a4cc-292b34ef8b3a"));
+     if (string.IsNullOrEmpty(categoryData.Name))
+     {
+          return Results.BadRequest("Category Name required");
+     }
+
+     if (categoryData.Name.Length > 2)
+     {
+          // Console.WriteLine($"{categoryData}");
+          var newCategory = new Category
+          {
+               CategoryId = Guid.NewGuid(),
+               Name = categoryData.Name,
+               Description = categoryData.Description,
+               CreatedAt = DateTime.UtcNow,
+          };
+          categories.Add(newCategory);
+          return Results.Created($"/api/categories/{newCategory.CategoryId}", newCategory);
+     }
+
+     else{
+          return Results.BadRequest("Minimum name value 3");
+     }
+
+});
+
+app.MapDelete("/api/categories/{categoryId:guid}", (Guid categoryId) =>
+{
+     var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == categoryId);
      if (foundCategory == null)
      {
-          return Results.NotFound("No categories found");
+          return Results.NotFound("Invalid categories");
      }
      categories.Remove(foundCategory);
-     return Results.NoContent();
+     return Results.Ok(new { message = "Category deleted successfully" });
 });
 
-app.MapPut("/api/categories", () =>
+app.MapPut("/api/categories/{categoryId:guid}", (Guid categoryId, Category categoryData) =>
 {
-     var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("d2719f0e-95d7-4f52-a4cc-292b34ef8b3a"));
+     if (categoryData == null)
+     {
+          return Results.BadRequest("Missing Category");
+     }
+     var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == categoryId);
      if (foundCategory == null)
      {
-          return Results.NotFound("No categories found");
+          return Results.BadRequest("Categories ID not found");
      }
-     foundCategory.Name = "Smart Phone";
-     foundCategory.Description = "Update description";
-     return Results.NoContent();
+     if (!string.IsNullOrWhiteSpace(categoryData.Name))
+     {
+          if (categoryData.Name.Length >= 2)
+          {
+               foundCategory.Name = categoryData.Name;
+          }
+          else
+          {
+               return Results.BadRequest("Add more character");
+          }
+     }
+     if (!string.IsNullOrEmpty(categoryData.Description))
+     {
+          foundCategory.Description = categoryData.Description;
+     }
+     // Simulate delete logic
+     Console.WriteLine($"Category with ID {categoryData.CategoryId} updated.");
+
+     return Results.Ok(new { message = "Updated successfully" });
+     // return Results.NoContent();
 });
 app.Run();
 
@@ -52,7 +98,7 @@ public record Category
 {
      public Guid CategoryId { get; set; }
      public string? Name { get; set; }
-     public string? Description { get; set; }
+     public string Description { get; set; } = string.Empty;
      public DateTime CreatedAt { get; set; }
 };
 
